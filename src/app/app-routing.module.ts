ad6e7b26debb1f705d1b6ab78b4ad34fa639962a
hectorucedo <hector.ucedo@gvcorp.com.py>
2022-05-25 17:37:40 -0400

    21. Editando el menú de navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const routes: Routes = [
-  { path: '', component: PortafolioComponent}, 
+  { path: 'home', component: PortafolioComponent}, 
   { path: 'about', component: AboutComponent}, 
   { path: 'item', component: ItemComponent}, 
   { path: '**', pathMatch: 'full', redirectTo: ''}, 
