d74b59b7bb8cd4eed479e84debb5fc152c04da2e
hectorucedo <hector.ucedo@gvcorp.com.py>
2022-05-25 17:22:18 -0400

    20. Creando rutas para nuestro sitio web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const routes: Routes = [
+  { path: '', component: PortafolioComponent}, 
+  { path: 'about', component: AboutComponent}, 
+  { path: 'item', component: ItemComponent}, 
+  { path: '**', pathMatch: 'full', redirectTo: ''}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
