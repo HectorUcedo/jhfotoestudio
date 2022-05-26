9941e195ab43e33d787f129c084b10d4913ca6b6
hectorucedo <hector.ucedo@gvcorp.com.py>
2022-05-26 12:37:45 -0400

    28. Usando el servicio para reemplazar información en la página web

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +10,7 @@ export class FooterComponent implements OnInit {
 
   anio: number = new Date().getFullYear();
 
-  constructor() { }
+  constructor( public _servicio: InfoPaginaService ) { }
 
   ngOnInit(): void {
   }
