de5435c32a86e697ded10795e9edd2d478fd14d1
hectorucedo <hector.ucedo@gvcorp.com.py>
2022-05-26 10:46:53 -0400

    26. Servicio - Información de la página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from './services/info-pagina.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jhfotoestudio';
+  //title = 'jhfotoestudio';
+
+  constructor( public infoPaginaService: InfoPaginaService ){
+    
+    // Leer el archivo Json
+    
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http'; // Importamos 
+
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,7 +26,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // Permite hacer petciones Http: Put, Get, Delete a servidores Rest por ejemplo
   ],
   providers: [],
   bootstrap: [AppComponent]
