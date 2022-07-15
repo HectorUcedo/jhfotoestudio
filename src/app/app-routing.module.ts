55c7979092ec822feebce0bb69cad6637e94a859
heucedo <hector.ucedo@itti.digital>
2022-07-15 15:47:39 -0400

    Modificamos el app-routing.modulo.ts para que direccione a home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent}, 
   { path: 'item/:id', component: ItemComponent}, 
   { path: 'search/:termino', component: SearchComponent}, 
-  { path: '**', pathMatch: 'full', redirectTo: ''}, 
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}, 
 ];
 
 @NgModule({
